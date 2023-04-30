e6d48ef03fc2769fc3ddaebf6a92340863305b81
Ritesh Choudhary <railsriteshc56@gmail.com>
2023-04-30 18:37:43 +0530

    Used firebase database for now to implement API calling.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ContactList from './components/ContactList';
 import './App.css';
 
 function App() {
-  const dummyContacts = [
-    {
-      id: 1,
-      firstName: "John",
-      lastName: "Wick",
-      email: "johnwick@gmail.com",
-      phoneNumber: "+91 9981239889",
-    },
-    {
-      id: 2,
-      firstName: "Jack",
-      lastName: "Tan",
-      email: "jacktan@gmail.com",
-      phoneNumber: "+91 99812394343",
-    },
-  ];
+  // const dummyContacts = [
+  //   {
+  //     id: 1,
+  //     firstName: "John",
+  //     lastName: "Wick",
+  //     email: "johnwick@gmail.com",
+  //     phoneNumber: "+91 9981239889",
+  //   },
+  //   {
+  //     id: 2,
+  //     firstName: "Jack",
+  //     lastName: "Tan",
+  //     email: "jacktan@gmail.com",
+  //     phoneNumber: "+91 99812394343",
+  //   },
+  // ];
+
+  const [contacts, setContacts] = useState([]);
+
+  function fetchContactsHandler(){
+    // for now using firebase database will replace with backend host once backend code is implemented.
+    fetch("https://testing-f9137-default-rtdb.firebaseio.com/contacts.json").then(response => {
+      return response.json();
+    }).then(data => {
+      const transformedData = []
+      for(let key in data){transformedData.push(data[key])}
+      setContacts(transformedData);
+    })
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Contacts</button>
+        <button onClick={fetchContactsHandler}>Fetch Contacts</button>
       </section>
       <section>
-        <ContactList contacts={dummyContacts} />
+        <ContactList contacts={contacts} />
       </section>
     </React.Fragment>
   );
